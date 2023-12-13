e3aaa88315b7a173cb34ec330d14007ab62bb971
Afx31 <post4wjm@gmail.com>
2023-12-13 21:43:16 +1100

    Opt out of storybook, will implement CSF later on

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,18 +3,18 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-
   "framework": {
     name: "@storybook/react-webpack5",
     options: {}
   },
-
+  features: {
+    storyStoreV7: false, // 👈 Opt out of on-demand story loading
+  },
   docs: {
     autodocs: true
   }
